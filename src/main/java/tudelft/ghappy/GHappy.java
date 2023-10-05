10881eb4394291992cca4f702fd84071da66ed20
Tatyana <breatninsideme@gmail.com>
2023-10-05 21:23:31 +0400

    Add GHappy tests and fix bugs in GHappy method

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -5,14 +5,15 @@ public class GHappy {
     public boolean gHappy(String str) {
         assert str!=null;
         for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
-                return false;
+            if(str.charAt(i) == 'g' || str.charAt(i) == 'G') {
+                if (i > 0 && (str.charAt(i-1) == 'g' || str.charAt(i-1) == 'G')) { return true; }
+                if (i+1 < str.length() && (str.charAt(i+1) == 'g' || str.charAt(i+1) == 'G')) { return true; }
+
             }
+
         }
 
-        return true;
+        return false;
 
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,41 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    private GHappy ghappy;
+
+    @BeforeEach
+    public void initialize(){
+        this.ghappy = new GHappy();
+    }
+
+    @Test
+    public void withoutG(){
+        Assertions.assertFalse(ghappy.gHappy("xxxxxx"));
+    }
+
+    @Test
+    public void WithSpace(){
+        Assertions.assertFalse(ghappy.gHappy("xxg gxxx"));
+    }
+
+    @Test
+    public void firstAndLastG(){
+        Assertions.assertFalse(ghappy.gHappy("gxxxxg"));
+    }
+
+    @Test
+    public void upperG(){
+        Assertions.assertTrue(ghappy.gHappy("xxGGxx"));
+    }
+
+    @Test
+    public void oneGAndTwoG(){
+        Assertions.assertTrue(ghappy.gHappy("gxxggx"));
+    }
+
 }
