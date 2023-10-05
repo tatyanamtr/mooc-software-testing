e9dcd9704e38aac049302da0c4fb9f7d43a9389f
Tatyana <breatninsideme@gmail.com>
2023-10-05 20:17:38 +0400

    Add BeforeEach into LeapYearTest

--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -1,31 +1,36 @@
 package tudelft.leapyear;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
+
+    private LeapYear ly;
+
+    @BeforeEach
+    public void initialize() {
+        this.ly = new LeapYear();
+    }
+
     @Test
     public void leapYearsThatAreNonCenturialYears() {
-        LeapYear ly = new LeapYear();
         boolean result = ly.isLeapYear(2016);
         Assertions.assertTrue(result);
     }
 
     @Test
     public void leapCenturialYears() {
-        LeapYear ly = new LeapYear();
         Assertions.assertTrue(ly.isLeapYear(2000));
     }
 
     @Test
     public void nonLeapCenturialYears() {
-        LeapYear ly = new LeapYear();
         Assertions.assertFalse(ly.isLeapYear(1500));
     }
 
     @Test
     public void nonLeapYears() {
-        LeapYear ly = new LeapYear();
         Assertions.assertFalse(ly.isLeapYear(2017));
     }
 }
