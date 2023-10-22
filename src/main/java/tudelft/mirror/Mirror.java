c0e9dd4b107046401ba6c51956904827a22bfc45
Tatyana <breatninsideme@gmail.com>
2023-10-22 20:14:39 +0400

    Fix bug in Mirror

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -7,7 +7,7 @@ public class Mirror {
 
         int begin = 0;
         int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
+        for (; end >= 0; begin++, end--) {
             if (string.charAt(begin) == string.charAt(end)) {
                 mirror += String.valueOf(string.charAt(end));
             }
